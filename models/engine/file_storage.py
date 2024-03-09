65334a9f952d93a793e6f111a5c5ebb4454844ca
Osama <elsharqawy2@gmail.com>
2024-03-09 14:50:25 +0200

    Console

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,22 +12,23 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
+    def all(self):
+        """ returns a dictionary contains all objects"""
+        return (FileStorage.__objects)
+
     def new(self, obj):
         """ creates an object"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
-    def all(self):
-        """ returns a dictionary contains all objects"""
-        return (FileStorage.__objects)
-
     def save(self):
         """ serialize the class objects to JSON file"""
-        serialized = {}
-        for key, obj in FileStorage.__objects.items():
-            serialized[key] = obj.to_dict()
+        myObjects = FileStorage.__objects
+        object_dictionary = {}
+        for obj in myObjects.keys():
+            object_dictionary[obj] = myObjects[obj].to_dict()
         with open(FileStorage.__file_path, "w") as file:
-            json.dump(serialized, file)
+            json.dump(object_dictionary, file)
 
     def reload(self):
         """deserialize the JSON file to objects if it exists only."""
