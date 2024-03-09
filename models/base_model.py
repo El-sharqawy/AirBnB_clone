80fe6dac11518e831d85f794b3dde8c1e37fe01b
Osama <elsharqawy2@gmail.com>
2024-03-09 13:13:42 +0200

    Console

---
+++ models/base_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Base Model Class
+"""
+
+from uuid import uuid4
+from datetime import datetime
+
+class BaseModel:
+    """BaseModel Class"""
+    def __init__(self, *args, **kwargs):
+        """Initialization"""
+        if kwargs:
+            pass
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+
+    def __str__(self):
+        """returns a string of the class data"""
+        string = "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return (string)
+
+    def save(self):
+        """Updates last time of the object"""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """returns a new dictionary copy of the original"""
+        newDict = self.__dict__.copy()
+        newDict["created_at"] = self.created_at.isoformat()
+        newDict["updated_at"] = self.updated_at.isoformat()
+        newDict["__class__"] = self.__class__.__name__
+        return (newDict)
