aa97f649c18a3f8a10113a8f43aac94df0056f5d
Osama <elsharqawy2@gmail.com>
2024-03-09 14:31:16 +0200

    Console

--- console.py
+++ console.py
@@ -4,22 +4,24 @@
 import cmd
 import os
 
+
 class HBNBCommand(cmd.Cmd):
-	"""the definition of HBnB command interperter"""
-	prompt = "(hbnb)"
+    """ the definition of HBnB command interperter"""
+    prompt = "(hbnb)"
 
-	def do_quit(self, arg):
-		""" Quit function that being called to exit the program"""
-		return True
+    def do_quit(self, arg):
+        """ Quit to exit the program"""
+        return True
 
-	def do_EOF(self, arg):
-		"""EOF signal to exit the program (ctrl+d)"""
-		print("")
-		return True
+    def do_EOF(self, arg):
+        """EOF signal to exit the program (ctrl+d)"""
+        print("")
+        return True
 
-	def emptyline(self):
-		"""handle empty lines to print nothing"""
-		pass
+    def emptyline(self):
+        """handle empty lines to print nothing"""
+        pass
+
 
 if __name__ == "__main__":
-	HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
